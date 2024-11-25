b7b8f8e0a8e4fc5eec9515b746b1990ea7eb4ca4
codehilltop95 <soumyadipchatterjee43522@gmail.com>
2024-11-26 01:42:21 +0530

    Change

--- sha256.py
+++ sha256.py
@@ -1,6 +1,5 @@
 from flask_smorest import Blueprint
 from flask.views import MethodView
-from passlib.hash import pbkdf2_sha256
 from firebase_admin import credentials, firestore,initialize_app
 from flask import jsonify
 from flask import request
@@ -15,13 +14,14 @@ cred = credentials.Certificate(json.loads(firebase_credentials_json))
 initialize_app(cred)
 db = firestore.client()  # Initialize Firestore or Realtime Database client
 # Define your Blueprint
+uuid=None
 @bl.route("/user", methods=["GET"])
 class security(MethodView):
     def get(self):
         # Extract UID from the query string
         uid = request.args.get("uid")
         timestamp = request.args.get("timestamp")
-
+        uuid=uid
         if not uid:
             return jsonify({"error": "Missing UID"}), 400
         return jsonify({
@@ -29,3 +29,9 @@ class security(MethodView):
             "uid": uid,
             "timestamp": timestamp
         }), 200
+@bl.route("/call",methods=["GET"])
+class callapi(MethodView):
+    def get(self):
+        return jsonify({
+            "uid":uuid,
+            "message":"UID fetched successfully" }),200
