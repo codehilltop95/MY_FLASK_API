d08b48c8a7bbaa028f275481728c23d2ea822c1c
codehilltop95 <soumyadipchatterjee43522@gmail.com>
2024-11-25 17:09:35 +0530

    New change

--- sha256.py
+++ sha256.py
@@ -5,7 +5,7 @@ from firebase_admin import credentials, firestore,initialize_app
 from flask import jsonify
 from requests import request
 bl=Blueprint("security",__name__)
-cred = credentials.Certificate(r"C:\Users\HOME\Desktop\api\.env\fir-auth-cb338-firebase-adminsdk-qwk97-fa554685e7.json")  # replace with your downloaded JSON file path
+cred = credentials.Certificate(r"C:\Users\HOME\Desktop\MY_FLASK_API\fir-auth-cb338-firebase-adminsdk-qwk97-fa554685e7.json")  # replace with your downloaded JSON file path
 initialize_app(cred)
 db = firestore.client()  # Initialize Firestore or Realtime Database client
 # Define your Blueprint
@@ -30,3 +30,7 @@ class security(MethodView):
             "uid": uid,
             "timestamp": timestamp
         }), 200
+@bl.route("/check")
+class check(MethodView):
+    def post(self):
+        return "HELLO API"
