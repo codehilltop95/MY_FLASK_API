b96666a85f76defd795cf590716b62223cd2de20
Soumyadip Chatterjee <soumyadipchatterjee43522@gmail.com>
2025-04-06 18:14:08 +0530

    Update sha256.py

--- sha256.py
+++ sha256.py
@@ -6,7 +6,7 @@ from flask import request
 import os,json
 from passlib.hash import pbkdf2_sha256
 bl=Blueprint("security",__name__)
-cred_path = os.environ.get('CRED_JSON')
+cred_path = os.environ.get(CRED_JSON)
 if not cred_path:
     raise ValueError("CRED_JSON environment variable not set!")
 cred = credentials.Certificate(cred_path)
